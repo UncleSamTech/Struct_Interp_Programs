a9fb932adafb418c040a1838c734815bfadab0d2
UncleSamTech <samueloel3486@gmail.com>
2023-07-05 19:06:13 -0600

    wip

--- elements.py
+++ elements.py
@@ -8,6 +8,43 @@ class element:
     def exp_square(self,n):
         return ((n+1) ** 2) + ((n * 2) ** 2)
         
+    def exercise1_1(self):
+        sum_vals = 5 + 3 + 4
+        compound_sum = ((2 * 4) + 6 - 4)  #(+ (* 2 4) (- 4 6))
+        a  = 3 # (define a 3)
+        b = a + 1 #(define b (+ a 1))
+        comp_proc = ((a + b) + (a * b))
+        if (b < a) and ((a * b) < b):
+            print(b)
+        else:
+            print(a)
+
+        if a == 4:
+            print(6)
+        elif b == 4:
+            print(6 + 7 + a) 
+        else:
+            print(25)
+
+        if b > a:
+            res = (b * a) + 2  #(+ 2 (if (> b a) b a))
+            print(res)
+        
+        if a > b:
+            print(a) * (a + 1) # (* (cond ((> a b) a)((< a b) b)(else -1))(+ a 1))
+        elif a < b:
+            print(b) * (a + 1)
+        else:
+            print(-1) * (a + 1)
+        
+    def exercise1_2(self):
+        numerator = (5 + 4 + (2 - (3 - (6 + (4/5)))))
+        denominator = 3 * (6 -2) (2 -7)
+        quot = numerator / denominator
+        print(quot)  #find predicate in the wiki
+
     
+
+                                                                                                                                                                                                            
 element_instance = element()
 element_instance.exp_square(5)
