32b797b34f943367a49680d6225cea32245f4e29
UncleSamTech <samueloel3486@gmail.com>
2023-07-26 14:52:25 -0600

    wip

--- ration_class.py
+++ ration_class.py
@@ -7,14 +7,23 @@ class rational:
     def create_ration(self):
         if self.num > 0 and self.denom > 0:
             return self.num / self.denom
+        elif self.num < 0 or self.denom < 0:
+            return -self.num / self.denom
         else:
-            print('invalid rational')
+            print('invalid')
+            
 
     def get_num(self):
         return self.num
     
     def get_den(self):
         return self.denom
+        
+    def display_ration(self):
+        if self.get_num() > 0 and self.get_den() > 0:
+            return str(self.get_num()) + '/' + str(self.get_den())
+        elif self.get_num() < 0 or self.get_den() < 0:
+            return str(-self.get_num()) + '/' + str(abs(self.get_den()))
 
    
         
