5fc8311b69f5e8f4a1bdb5e396b724acd4710a12
UncleSamTech <samueloel3486@gmail.com>
2023-08-01 14:48:37 -0600

    wip

--- alyssa.py
+++ alyssa.py
@@ -3,37 +3,35 @@ class alyssa:
         self.resist1 = None
         self.resist2 = None
         self.para_equiv_res = None
-        self.lower_bound_x = None
-        self.upper_bound_x = None
-        self.lower_bound_y = None
-        self.upper_bound_y = None
+        self.max_bound = None
+        self.min_bound = None
         self.x = None
         self.y = None
 
     def compute_par_res(self):
         return 1 / ((1 / self.resist1) + (1/self.resist2))
     
-    def create_bound(x,y):
-        pass
+    def create_lower_bound(val):
+        return min(val)
+        
+    def create_upper_bound(val):
+        return max(val)
     
     def make_interval(self,x,y):
-        min_bound = self.lower_bound_x + self.lower_bound_y
-        max_bound = self.upper_bound_x + self.upper_bound_y
-        return min_bound, max_bound
+        self.min_bound = self.create_lower_bound(x) + self.create_upper_bound(y)
+        self.max_bound = self.create_upper_bound(x) + self.create_lower_bound(y)
+        return self.min_bound, self.max_bound
     
-    def mult_interval(self):
-        p1 = self.lower_bound_x * self.lower_bound_y
-        p2 = self.lower_bound_x * self.upper_bound_y
-        p3 = self.upper_bound_x * self.lower_bound_y
-        p4 = self.upper_bound_x * self.upper_bound_y
+    def mult_interval(self,x,y):
+        p1 = self.create_lower_bound(x) * self.create_lower_bound(y)
+        p2 = self.create_lower_bound(x) * self.create_upper_bound(y)
+        p3 = self.create_upper_bound(x) * self.create_lower_bound(y)
+        p4 = self.create_upper_bound(x) * self.create_lower_bound(y)
 
         return self.make_interval(min(p1,p2,p3,p4), max(p1,p2,p3,p4))
 
     def div_interv(self,x,y):
-        return self.mult_interval(x,self.make_interval((self.upper_bound_y / 1.0), (self.lower_bound_y) / 1.0))
-
-
-    
+        return self.mult_interval(x,self.make_interval((self.create_upper_bound(y) / 1.0), (self.create_lower_bound(y)) / 1.0))
 
     def interv_arith(self):
         pass
