e237ba7c47c7250b0434eb80907d0729a76cb6ce
UncleSamTech <samueloel3486@gmail.com>
2023-08-03 14:49:33 -0600

    wip

--- hierachy.py
+++ hierachy.py
@@ -1,7 +1,6 @@
 class hierachy:
     def __init__(self):
         self.squares = [1,4,9,16,25]
-        pass
 
     def list_ref(self,items,n):
         return items[n] if n == 0 else self.list_ref(items[1:],n-1)
