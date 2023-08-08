bd8ac13019b1d0beae17f25f8e78e016a1716146
UncleSamTech <samueloel3486@gmail.com>
2023-08-08 09:48:50 -0600

    wip

--- list_parity.py
+++ list_parity.py
@@ -6,14 +6,9 @@ class list_parity:
         return self.list_val
     
     def even_odd_parity(self,*args):
-        list1 = []
-        list2 = []
-        for i in args:
-            if i % 2 == 0:
-                list1.append(i)
-            else:
-                list2.append(i)
-        return list1,list2
+        even_val = [i for i in args if i % 2 == 0]
+        odd_val = [j for j in args if j % 2 != 0]
+        return even_val,odd_val
     
     def scale_list_items(self,items,factor):
         return [] if items is None or len(items) < 1 else ([i * factor for i in items[0:1]]) + self.scale_list_items(items[1:],factor) 
