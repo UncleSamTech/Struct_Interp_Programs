cc0efd3112ca7763bf8b5a89b22945a6dda29d2c
UncleSamTech <samueloel3486@gmail.com>
2023-08-11 15:15:45 -0600

    wip

--- trees.py
+++ trees.py
@@ -7,7 +7,9 @@ class trees:
     def count_leaves(self, list_val):
         if list_val is None or len(list_val) < 1:
             return 0
-        elif len(list_val) == 1 or any(isinstance(i,list) for i in list_val) is False:
+        elif len(list_val) == 1 or any(isinstance(i,list) for i in list_val) is False or any(isinstance(k,dict) for k in list_val):
             return 1
+        elif any(isinstance(m,list) for m in list_val):
+            return (len(i) for i in list_val)
         else:
             return self.count_leaves(list_val[0:1]) + self.count_leaves(list_val[1:])
